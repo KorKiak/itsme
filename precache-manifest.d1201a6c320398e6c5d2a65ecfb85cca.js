24fa06defd164e13a5d563ba11d552bc55616564
KorKiak <tanit_kositsri@hotmail.com>
2019-02-16 11:23:46 +0700

    Updates

---
+++ precache-manifest.d1201a6c320398e6c5d2a65ecfb85cca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/itsme/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7ebfed553192a75cac0a",
+    "url": "/itsme/static/js/runtime~main.7ebfed55.js"
+  },
+  {
+    "revision": "b43c79f793be2f1776c1",
+    "url": "/itsme/static/js/main.b43c79f7.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/itsme/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "b43c79f793be2f1776c1",
+    "url": "/itsme/static/css/main.e36eec3e.chunk.css"
+  },
+  {
+    "revision": "53d6b8826c2dc822728bc2ee8b957e06",
+    "url": "/itsme/index.html"
+  }
+];
